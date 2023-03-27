a9a197641999840496bf073e0c545487d6a536a6
Kang seungyeon <77188916+KangSYeon@users.noreply.github.com>
2023-03-27 13:35:58 +0900

    Update Assignment1.c
    Error 없앤 거

--- Assignment1.c
+++ Assignment1.c
@@ -6,6 +6,9 @@
 #define STsize 1000 //size of string table
 #define HTsize 100 //size of hash table
 // more define variables��
+//김제인 - 추가 매크로 함수 정의
+#define isLetter(x) (((x) >= 'a' && (x) <= 'z') || ((x) >= 'A' && (x) <= 'Z') || (x) == '_')
+#define isDigit(x) ((x) >= '0' && (x) <= '9')
 
 typedef struct HTentry* HTpointer;
 typedef struct HTentry {
@@ -29,6 +32,9 @@ int hash_code; // 식별자 해시코드
 int found; // 식별자가 이미 존재하는 경우를 위한 flag
 int same; // 식별자의 첫번째 인덱스(starting index)
 
+int nextid;
+int nextfree;
+
 FILE* fp; //to be a pointer to FILE
 char input;
 
@@ -41,13 +47,25 @@ void initialize()
 	input = fgetc(fp);
 }
 
-
 //KJI
 // Skip Seperators - skip over strings of spaces,tabs,newlines, . , ; : ? !
 // if illegal seperators,print out error message.
 
 void SkipSeperators()
 {
+	char* ptr = strchr(seperators, input);
+
+	// 파일 탐색 중인데 문자도 숫자도 아닌 입력인 경우
+	while (input != EOF && (isLetter(input) || isDigit(input)))
+	{
+		// 정의된 구분자도 아닌 경우
+		if (ptr == NULL)
+		{
+			err = illsp;
+			PrintError(err);
+		}
+		input = fgetc(fp); // 다음 글자로 반복
+	} // 문자, 숫자면 while문 탈출
 }
 
 
@@ -56,8 +74,29 @@ void SkipSeperators()
 // associated with each hashcode,but only for non-empty lists.
 // Print out the number of characters used up in ST.
 
-void PrintHStable()
+int PrintHStable()
 {
+	int HInd;
+	printf("[[HASH TABLE]]\n\n");
+	for (int h = 0; h < HTsize; h++) {
+		if (HT[h] == NULL) //HT 비어있을때
+			continue;
+		else { //HT 비어있지않을때
+			printf("Hash Code %d : ", h); //Hash Code x : 까지 출력
+			HTentry K = *HT[h];
+			while (K.next != NULL) { //HTentry의 다음 pointer가 null일때까지 반복
+				HInd = K.index;
+				while (ST[HInd] != '\0') {
+					printf("%c", ST[HInd]);
+					HInd++;
+				}
+				printf("   ");
+				K = *(K.next);
+			}
+			printf("\n");
+		}
+	}
+	return nextfree;
 }
 
 
@@ -73,7 +112,7 @@ void PrintError(ERRORtypes err)
 	switch (err) {
 	case overst:
 		printf("***Error***  OVERFLOW\n");
-		PrintHStable();
+		PrintHStable(3); //에러 방지용으로 10 대입
 		abort();
 	case illid:
 		printf("...Error...  ILLID\n");
@@ -93,6 +132,14 @@ void PrintError(ERRORtypes err)
 
 void ReadID()
 {
+	// 파일 탐색 중인데 문자도 숫자도 아닌 입력인 경우
+	while (input != EOF && (isLetter(input) || isDigit(input)))
+	{
+		nextfree++;
+		ST[nextfree] = input;
+		input = fgetc(fp); // 다음 글자로 반복
+	}
+	SkipSeperators();
 }
 
 
@@ -106,7 +153,6 @@ void ComputeHS(int nid, int nfree)
 	for (i = nid; i < nfree - 1; i++)
 		c += (int)ST[i]; // 반복문 이용해서 각 문자 값 더하기
 	hash_code = c % HTsize; // 해시테이블 크기로 나누어서 해시코드 계산
-
 }
 
 
@@ -154,7 +200,7 @@ void LookupHS(int nid, int hscode)
 
 void ADDHT(int hscode)
 {
-	HTpointer htp; 
+	HTpointer htp;
 	HTentry hte;
 	htp = HT[hscode];
 	hte.index = same;
@@ -164,7 +210,7 @@ void ADDHT(int hscode)
 		HT[hscode] = htp;
 	}
 	else {
-		while(htp->next != NULL) //until next is NULL
+		while (htp->next != NULL) //until next is NULL
 			htp = htp->next;
 		htp->next = &hte;
 	}
@@ -181,10 +227,18 @@ If not matched, add a new element to the list, pointing to new identifier.
 Print the identifier, its index in ST, and whether it was entered or present.
 Print out the hashtable, and number of characters used up in ST
 */
+void PrintHeading()
+{
+	printf("-----------          ----------\n");
+	printf("Index in ST          identifier\n");
+	printf("-----------          ----------\n");
+}
+
 
 int main()
 {
-	int i;
+	int i; //?
+	int sum = 0;
 
 	PrintHeading();
 	initialize();
@@ -193,28 +247,32 @@ int main()
 		err = noerror;
 		SkipSeperators();
 		ReadID();
+		
+		//(강승연)선언되지 않은 변수 사용으로 인한 에러 발생 - 주석처리
 
-		//(���¿�)������� ���� ���� ������� ���� ���� �߻� - �ּ�ó��
-		/*
 		if (input != EOF && err != illid) {
-			if (nextfree == STsize) {
-				// print error message
-			}
+		   if (nextfree == STsize) {
+			  // print error message
+		   }
 
-			ST[nextfree++] = '\0';
-			ComputeHS(nextid, nextfree);
-			LookupHS(nextid, hashcode);
+		   ST[nextfree++] = '\0';
+		   ComputeHS(nextid, nextfree);
+		   LookupHS(nextid, hash_code);
 
-			if (!found) {
-				// print message
-				ADDHT(hashcode);
-			}
-			else {
-				// print message
-			}
+		   if (!found) {
+			  // print message
+			  ADDHT(hash_code);
+		   }
+		   else {
+			  // print message
+		   }
 		}
-		*/
+		
 	}
-
-	PrintHStable();
+	PrintHStable(sum);
+	printf("< %d characters are used in the string table >", sum);
 }
+
+
+
+
