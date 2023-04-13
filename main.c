f7302af3dee315b464f51a101c1f29492fa41110
Kang seungyeon <77188916+KangSYeon@users.noreply.github.com>
2023-04-13 15:08:21 +0900

    pure main.c from prof

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "tn.h"
+#include "global.h"
+
+void main() {
+	enum tnumber tn;
+	//print header
+
+	//**code from prof**
+
+	//while ((tn = yylex()) != EOF) {
+	//	printtoken(tn);
+	//}
+	//if (cErrors == 0) then
+	//	printf("No errors defected ")
+	//	Else
+	//	printf("%d errors detected", cErrors);
+	//end if
+}
